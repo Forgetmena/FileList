1030bea855e50b9ddf415f58b36926cd2e105126
Forgetmena <forgetmenas@outlook.com>
2026-04-06 14:37:57 +0800

    6th commit

--- FileList_Method.c
+++ FileList_Method.c
@@ -2,7 +2,7 @@
 #include "FileList_Method.h"
 
 //创建一个空链表
-void createEmptyFileList(FILELISTPTR* headPtrPtr) {
+void createEmptyFileList(FILELISTPTR* headPtrPtr, FILELISTPTR* lastPtrPtr) {
     (*headPtrPtr) = malloc(sizeof(FILELIST));
     if (*headPtrPtr == NULL) {
         printf("内存分配失败\n");
@@ -11,9 +11,10 @@ void createEmptyFileList(FILELISTPTR* headPtrPtr) {
     (*headPtrPtr)->fileName = NULL;
     (*headPtrPtr)->prevPtr = (*headPtrPtr);
     (*headPtrPtr)->nextPtr = (*headPtrPtr); 
+    (*lastPtrPtr) = (*headPtrPtr);
 }
 
-//功能一：遍历函数，如果存在则返回该节点的指针，不存在则输出提示信息并返回headPtr
+//功能一：遍历函数，如果存在则返回该节点的指针，不存在则输出提示信息并返回NULL
 FILELISTPTR findFileNodePtr(FILELISTPTR headPtr, char* fileName) {
     FILELISTPTR currentPtr = headPtr -> nextPtr;
     while (currentPtr != headPtr)
@@ -24,15 +25,17 @@ FILELISTPTR findFileNodePtr(FILELISTPTR headPtr, char* fileName) {
         }
         currentPtr = currentPtr->nextPtr;
     }
-    printf("文件不存在\n");
     return headPtr;
 }
 
-//功能二：查看历史文件并且将文件提到第一位，并且删除原本的历史记录
-void moveHistoryFileToFirst(FILELISTPTR* headPtrPtr, char* fileName) {
+//功能二：查看历史文件并且将文件提到第一位（注意对lastPtr的处理）,并且删除原本的历史记录
+void moveHistoryFileToFirst(FILELISTPTR* headPtrPtr, FILELISTPTR* lastPtrPtr, char* fileName) {
     FILELISTPTR currentPtr = findFileNodePtr((*headPtrPtr), fileName);
     if (currentPtr == (*headPtrPtr)) {
         return;
+    }
+    if (currentPtr == (*lastPtrPtr)) {
+        (*lastPtrPtr) = currentPtr->prevPtr;
     }
     currentPtr->prevPtr->nextPtr = currentPtr->nextPtr;
     currentPtr->nextPtr->prevPtr = currentPtr->prevPtr;
@@ -43,18 +46,23 @@ void moveHistoryFileToFirst(FILELISTPTR* headPtrPtr, char* fileName) {
 }
 
 //功能三：添加历史文件，如果已经存在则调用功能二，如果不存在则添加到第一位
-void addHistoryFile(FILELISTPTR* headPtrPtr, FILELISTPTR newPtr) {
+void addHistoryFile(FILELISTPTR* headPtrPtr, FILELISTPTR* lastPtrPtr, FILELISTPTR newPtr) {
     FILELISTPTR currentPtr = findFileNodePtr((*headPtrPtr), newPtr->fileName);
     if (currentPtr == (*headPtrPtr)) {
         newPtr->nextPtr = (*headPtrPtr)->nextPtr;
         (*headPtrPtr)->nextPtr->prevPtr = newPtr;
         (*headPtrPtr)->nextPtr = newPtr;
         newPtr->prevPtr = (*headPtrPtr);
-    } 
+        (*lastPtrPtr) = (*lastPtrPtr)->nextPtr;
+        (*headPtrPtr)->prevPtr = (*lastPtrPtr);
+    } else {
+        moveHistoryFileToFirst(headPtrPtr, lastPtrPtr, newPtr->fileName);
+    }
+    RemoveBoundaryInvalid(headPtrPtr, lastPtrPtr);
 }
 
 //判断链表是否超出最大存储量10，如果超出则删除最后一个节点
-void isBoundaryValid(FILELISTPTR* headPtrPtr) {
+void RemoveBoundaryInvalid(FILELISTPTR* headPtrPtr, FILELISTPTR* lastPtrPtr) {
     int cnt = 0;
     FILELISTPTR currentPtr = (*headPtrPtr)->nextPtr;
     while (currentPtr != (*headPtrPtr)) {
@@ -62,10 +70,12 @@ void isBoundaryValid(FILELISTPTR* headPtrPtr) {
         currentPtr = currentPtr->nextPtr;
     } 
     if (cnt > 10) {
-        FILELISTPTR lastPtr = (*headPtrPtr)->prevPtr;
-        lastPtr->prevPtr->nextPtr = (*headPtrPtr);
-        (*headPtrPtr)->prevPtr = lastPtr->prevPtr;
-        free(lastPtr);
+        FILELISTPTR tempPtr = (*lastPtrPtr);
+        (*lastPtrPtr) = (*lastPtrPtr)->prevPtr;
+        (*lastPtrPtr)->nextPtr = (*headPtrPtr);
+        (*headPtrPtr)->prevPtr = (*lastPtrPtr);
+        free(tempPtr->fileName);
+        free(tempPtr);
     }
     return; 
 }
--- FileList_Method.h
+++ FileList_Method.h
@@ -3,8 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-void createEmptyFileList(FILELISTPTR* headPtrPtr);
+void createEmptyFileList(FILELISTPTR* headPtrPtr, FILELISTPTR* lastPtrPtr);
 
 FILELISTPTR findFileNodePtr(FILELISTPTR headPtr, char* fileName);
 
-void moveHistoryFileToFirst(FILELISTPTR* headPtrPtr, char* fileName);
+void moveHistoryFileToFirst(FILELISTPTR* headPtrPtr, FILELISTPTR* lastPtrPtr, char* fileName);
+
+void addHistoryFile(FILELISTPTR* headPtrPtr, FILELISTPTR* lastPtrPtr, FILELISTPTR newPtr);
+
+void RemoveBoundaryInvalid(FILELISTPTR* headPtrPtr, FILELISTPTR* lastPtrPtr);
